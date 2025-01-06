89d81b4f1f4a8327b408006ec53c371103e03d80
nikhileatha <136676963+nikhileatha@users.noreply.github.com>
2025-01-06 10:33:26 +0530

    Add files via upload

---
+++ core java/PS001.java
@@ -0,0 +1,12 @@
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String address;
+
+    public Employee() {
+        this.id = 0;
+        this.firstName = "";
+        this.lastName = "";
+        this.address = "";
+    }
