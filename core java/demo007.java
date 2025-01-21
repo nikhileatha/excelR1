b389c2b9c22083114e028b9b84ad771bb315e410
nikhileatha <136676963+nikhileatha@users.noreply.github.com>
2025-01-21 11:37:39 +0530

    Update and rename demo7.java to demo007.java
    printing name and age

--- core java/demo007.java
+++ core java/demo007.java
@@ -1,6 +1,6 @@
 import java.util.Scanner; 
 
-public class demo7 {
+public class demo007 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
