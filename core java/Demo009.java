ae065411f70f288cc103d8059188b8387f5308c0
nikhileatha <136676963+nikhileatha@users.noreply.github.com>
2025-02-01 11:05:49 +0530

    Add files via upload

---
+++ core java/Demo009.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Demo009 {
+    String Cust_Name = "Sandeep";
+    Double Balance = 10000.00;
+    void accept_details(){
+        System.out.println("enter ur name, account no");
+        Scanner sc = new Scanner(System.in);
+        Cust_Name = sc.nextLine();
+        sc.nextLong();
+        // Balance = sc.nextDouble();
+    }
+    void Display_details(){
+        System.out.println("Hello "+Cust_Name);
+        System.out.println("ur balance is "+ Balance);
+    }
+    public static void main(String[] args){
+        Demo009 obj = new Demo009();
+        obj.accept_details();
+        obj.Display_details();
+    }
+}
