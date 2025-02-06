8fd3cb4cf39b1542516328b161b5fb61addd2151
nikhileatha <136676963+nikhileatha@users.noreply.github.com>
2025-02-06 11:05:56 +0530

    printing index values of an array

---
+++ core java/demo066.java
@@ -0,0 +1,10 @@
+public class demo066 {
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40, 50};
+
+        System.out.println("Index\tValue");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(i + "\t" + a[i]);
+        }
+    }
+}
