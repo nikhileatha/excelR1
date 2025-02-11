a525506d6cf92e4a43db6002a13e237e3e9df9c1
nikhileatha <136676963+nikhileatha@users.noreply.github.com>
2025-02-11 11:24:22 +0530

    sorting an array without predefine method

--- core java/demo074.java
+++ core java/demo074.java
@@ -1,23 +1,27 @@
- public class demo074 {
-        public static void main(String[] args) {
-            int[] array = {5, 2, 9, 1, 4, 6};
-            bubbleSort(array);
-            System.out.println("Sorted array:");
-            for (int i : array) {
-                System.out.print(i + " ");
-            }
+import java.util.Scanner;
+
+public class demo074 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter size of an array: ");
+        int n = sc.nextInt();
+        System.out.print("Enter elements in an array: ");
+        int array[] = new int[n];
+        for(int i=0;i<array.length;i++){
+            array[i]=sc.nextInt();
         }
-    
-        public static void bubbleSort(int[] array) {
-            int n = array.length;
-            for (int i = 0; i < n - 1; i++) {
-                for (int j = 0; j < n - 1 - i; j++) {
-                    if (array[j] > array[j + 1]) {
-                        int temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
-                    }
+        for(int i=0;i<array.length;i++){
+            for(int j=i+1;j<array.length;j++){
+                if(array[i]>array[j]){
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+
                 }
             }
+            System.out.println(array[i]);
         }
+      
+        sc.close();
     }
+}
